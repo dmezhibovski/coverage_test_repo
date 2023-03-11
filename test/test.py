70944713306fa21d2a8baa264d229898b9e9ee38
Daniel Mezhibovski <45374814+dmezhibovski@users.noreply.github.com>
2023-03-11 10:41:27 -0500

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,9 @@
+import unittest
+from a import adder 
+
+class TestClass(unittest.TestCase):
+    def testAdder(self):
+        self.assertEqual(adder(1,2),3)
+        self.assertEqual(adder(0,0),0)
+        self.assertEqual(adder(1,-1),0)
+        self.assertEqual(adder(1,-2),-1)
